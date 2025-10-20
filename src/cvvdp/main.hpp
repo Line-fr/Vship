6de0d7ab5f07dcdc17bdc303319f0432afc6c19b
line <linefremery@gmail.com>
2025-10-20 09:29:06 +0200

    improve gpu parallelization with a single handler for cvvdp by using 2 streams internally. This is especially beneficial since CVVDP is a video metric and only one handler may be created, forcing -g to 1

--- src/cvvdp/main.hpp
+++ src/cvvdp/main.hpp
@@ -14,14 +14,20 @@
 
 namespace cvvdp{
 
-double CVVDPprocess(const uint8_t *dstp, int64_t dststride, TemporalRing temporalRing1, TemporalRing temporalRing2, DisplayModel* model, int64_t maxshared, hipStream_t stream){
+double CVVDPprocess(const uint8_t *dstp, int64_t dststride, TemporalRing temporalRing1, TemporalRing temporalRing2, DisplayModel* model, int64_t maxshared, hipStream_t stream1, hipStream_t stream2, hipEvent_t event){
     int64_t width = temporalRing1.width;
     int64_t height = temporalRing1.height;
 
-    int allocatedPlanes = 8;
+    int allocatedPlanes = 4;
     int gaussianPyrSizeMultiplier = 2; //each plane will get twice the normal size so that we can fit the pyramid next to them
     float* mem_d;
-    hipError_t erralloc = hipMallocAsync(&mem_d, sizeof(float)*allocatedPlanes*width*height * gaussianPyrSizeMultiplier, stream);
+    hipError_t erralloc = hipMallocAsync(&mem_d, sizeof(float)*allocatedPlanes*width*height * gaussianPyrSizeMultiplier, stream1);
+    if (erralloc != hipSuccess){
+        throw VshipError(OutOfVRAM, __FILE__, __LINE__);
+    }
+    //second stream
+    float* mem_d2;
+    erralloc = hipMallocAsync(&mem_d2, sizeof(float)*allocatedPlanes*width*height * gaussianPyrSizeMultiplier, stream2);
     if (erralloc != hipSuccess){
         throw VshipError(OutOfVRAM, __FILE__, __LINE__);
     }
@@ -31,16 +37,17 @@ double CVVDPprocess(const uint8_t *dstp, int64_t dststride, TemporalRing tempora
     float* YV_sustained1 = mem_d + 2*width*height*gaussianPyrSizeMultiplier;
     float* Y_transient1 = mem_d + 3*width*height*gaussianPyrSizeMultiplier;
 
-    float* Y_sustained2 = mem_d + 4*width*height*gaussianPyrSizeMultiplier;
-    float* RG_sustained2 = mem_d + 5*width*height*gaussianPyrSizeMultiplier;
-    float* YV_sustained2 = mem_d + 6*width*height*gaussianPyrSizeMultiplier;
-    float* Y_transient2 = mem_d + 7*width*height*gaussianPyrSizeMultiplier;
+    float* Y_sustained2 = mem_d2;
+    float* RG_sustained2 = mem_d2 + width*height*gaussianPyrSizeMultiplier;
+    float* YV_sustained2 = mem_d2 + 2*width*height*gaussianPyrSizeMultiplier;
+    float* Y_transient2 = mem_d2 + 3*width*height*gaussianPyrSizeMultiplier;
 
     //let's get the temporal channel out of the temporal ring!
-    computeTemporalChannels(temporalRing1, Y_sustained1, RG_sustained1, YV_sustained1, Y_transient1, stream);
-    computeTemporalChannels(temporalRing2, Y_sustained2, RG_sustained2, YV_sustained2, Y_transient2, stream);
+    computeTemporalChannels(temporalRing1, Y_sustained1, RG_sustained1, YV_sustained1, Y_transient1, stream1);
+    computeTemporalChannels(temporalRing2, Y_sustained2, RG_sustained2, YV_sustained2, Y_transient2, stream2);
 
-    hipFreeAsync(mem_d, stream);
+    hipFreeAsync(mem_d, stream1);
+    hipFreeAsync(mem_d2, stream2);
 
     return 10.;
 }
@@ -55,7 +62,9 @@ class CVVDPComputingImplementation{
     int64_t resize_width = 0;
     int64_t resize_height = 0;
     int maxshared = 0;
-    hipStream_t stream = 0;
+    hipStream_t stream1 = 0;
+    hipStream_t stream2 = 0;
+    hipEvent_t event;
 public:
     void init(int64_t width, int64_t height, float fps, bool resizeToDisplay, std::string model_key){
         model = new DisplayModel(model_key);
@@ -83,7 +92,9 @@ public:
         temporalRing1.init(fps, resize_width, resize_height);
         temporalRing2.init(fps, resize_width, resize_height);
 
-        hipStreamCreate(&stream);
+        hipStreamCreate(&stream1);
+        hipStreamCreate(&stream2);
+        hipEventCreate(&event);
 
         int device;
         hipDeviceProp_t devattr;
@@ -96,7 +107,9 @@ public:
         temporalRing1.destroy();
         temporalRing2.destroy();
         delete model;
-        hipStreamDestroy(stream);
+        hipStreamDestroy(stream1);
+        hipStreamDestroy(stream2);
+        hipEventDestroy(event);
     }
     template <InputMemType T>
     void loadImageToRing(const uint8_t *srcp1[3], const uint8_t *srcp2[3], int64_t stride, int64_t stride2){
@@ -109,7 +122,13 @@ public:
         }
         //allocate memory to send the raw to gpu
         float* mem_d;
-        hipError_t erralloc = hipMallocAsync(&mem_d, std::max(stride, stride2)*source_height + resizeBufferBytes, stream);
+        hipError_t erralloc = hipMallocAsync(&mem_d, (std::max(stride, stride2)*source_height + resizeBufferBytes), stream1);
+        if (erralloc != hipSuccess){
+            throw VshipError(OutOfVRAM, __FILE__, __LINE__);
+        }
+        //for second stream
+        float* mem_d2;
+        erralloc = hipMallocAsync(&mem_d2, (std::max(stride, stride2)*source_height + resizeBufferBytes), stream2);
         if (erralloc != hipSuccess){
             throw VshipError(OutOfVRAM, __FILE__, __LINE__);
         }
@@ -117,6 +136,10 @@ public:
         //defined onnly if is_resized is true
         float* tempResize = (float*)((uint8_t*)mem_d + std::max(stride, stride2)*source_height); //of size resize plane
         float* tempStrideEliminated = tempResize + resize_width*resize_height; //of size source plane (float)
+
+        //for second stream
+        float* tempResize2 = (float*)((uint8_t*)mem_d2 + std::max(stride, stride2)*source_height); //of size resize plane
+        float* tempStrideEliminated2 = tempResize2 + resize_width*resize_height; //of size source plane (float)
 
         //free up a plane by increasing history to 1, we can now edit the frame 0 which is blank
         temporalRing1.rotate();
@@ -128,47 +151,48 @@ public:
         float* src2_d[3] = {encoded_ptr, encoded_ptr+resize_width*resize_height, encoded_ptr+2*resize_width*resize_height};
 
         //we put the frame's planes on GPU
-        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp1[0]), stride * source_height, stream));
-        strideEliminator<T>(tempStrideEliminated, mem_d, stride, source_width, source_height, stream);
-        rgb_to_linrgb(is_resized ? tempStrideEliminated : src1_d[0], source_width*source_height, stream);
-        if (is_resized) resizePlane(src1_d[0], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream);
+        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp1[0]), stride * source_height, stream1));
+        strideEliminator<T>(tempStrideEliminated, mem_d, stride, source_width, source_height, stream1);
+        rgb_to_linrgb(is_resized ? tempStrideEliminated : src1_d[0], source_width*source_height, stream1);
+        if (is_resized) resizePlane(src1_d[0], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream1);
         
-        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp1[1]), stride * source_height, stream));
-        strideEliminator<T>(tempStrideEliminated, mem_d, stride, source_width, source_height, stream);
-        rgb_to_linrgb(is_resized ? tempStrideEliminated : src1_d[1], source_width*source_height, stream);
-        if (is_resized) resizePlane(src1_d[1], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream);
+        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp1[1]), stride * source_height, stream1));
+        strideEliminator<T>(tempStrideEliminated, mem_d, stride, source_width, source_height, stream1);
+        rgb_to_linrgb(is_resized ? tempStrideEliminated : src1_d[1], source_width*source_height, stream1);
+        if (is_resized) resizePlane(src1_d[1], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream1);
 
-        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp1[2]), stride * source_height, stream));
-        strideEliminator<T>(tempStrideEliminated, mem_d, stride, source_width, source_height, stream);
-        rgb_to_linrgb(is_resized ? tempStrideEliminated : src1_d[2], source_width*source_height, stream);
-        if (is_resized) resizePlane(src1_d[2], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream);
+        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp1[2]), stride * source_height, stream1));
+        strideEliminator<T>(tempStrideEliminated, mem_d, stride, source_width, source_height, stream1);
+        rgb_to_linrgb(is_resized ? tempStrideEliminated : src1_d[2], source_width*source_height, stream1);
+        if (is_resized) resizePlane(src1_d[2], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream1);
 
-        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp2[0]), stride2 * source_height, stream));
-        strideEliminator<T>(tempStrideEliminated, mem_d, stride2, source_width, source_height, stream);
-        rgb_to_linrgb(is_resized ? tempStrideEliminated : src2_d[0], source_width*source_height, stream);
-        if (is_resized) resizePlane(src2_d[0], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream);
+        GPU_CHECK(hipMemcpyHtoDAsync(mem_d2, (void*)(srcp2[0]), stride2 * source_height, stream2));
+        strideEliminator<T>(tempStrideEliminated2, mem_d2, stride2, source_width, source_height, stream2);
+        rgb_to_linrgb(is_resized ? tempStrideEliminated2 : src2_d[0], source_width*source_height, stream2);
+        if (is_resized) resizePlane(src2_d[0], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream2);
         
-        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp2[1]), stride2 * source_height, stream));
-        strideEliminator<T>(tempStrideEliminated, mem_d, stride2, source_width, source_height, stream);
-        rgb_to_linrgb(is_resized ? tempStrideEliminated : src2_d[1], source_width*source_height, stream);
-        if (is_resized) resizePlane(src2_d[1], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream);
+        GPU_CHECK(hipMemcpyHtoDAsync(mem_d2, (void*)(srcp2[1]), stride2 * source_height, stream2));
+        strideEliminator<T>(tempStrideEliminated2, mem_d2, stride2, source_width, source_height, stream2);
+        rgb_to_linrgb(is_resized ? tempStrideEliminated2 : src2_d[1], source_width*source_height, stream2);
+        if (is_resized) resizePlane(src2_d[1], tempResize2, tempStrideEliminated2, source_width, source_height, resize_width, resize_height, stream2);
         
-        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp2[2]), stride2 * source_height, stream));
-        strideEliminator<T>(tempStrideEliminated, mem_d, stride2, source_width, source_height, stream);
-        rgb_to_linrgb(is_resized ? tempStrideEliminated : src2_d[2], source_width*source_height, stream);
-        if (is_resized) resizePlane(src2_d[2], tempResize, tempStrideEliminated, source_width, source_height, resize_width, resize_height, stream);
+        GPU_CHECK(hipMemcpyHtoDAsync(mem_d, (void*)(srcp2[2]), stride2 * source_height, stream2));
+        strideEliminator<T>(tempStrideEliminated2, mem_d2, stride2, source_width, source_height, stream2);
+        rgb_to_linrgb(is_resized ? tempStrideEliminated2 : src2_d[2], source_width*source_height, stream2);
+        if (is_resized) resizePlane(src2_d[2], tempResize2, tempStrideEliminated2, source_width, source_height, resize_width, resize_height, stream2);
 
         //colorspace conversion
-        linrgb_to_dkl(src1_d, resize_width*resize_height, stream);
-        linrgb_to_dkl(src2_d, resize_width*resize_height, stream);
+        linrgb_to_dkl(src1_d, resize_width*resize_height, stream1);
+        linrgb_to_dkl(src2_d, resize_width*resize_height, stream2);
 
-        hipFreeAsync(mem_d, stream);
+        hipFreeAsync(mem_d, stream1);
+        hipFreeAsync(mem_d2, stream2);
         //and we are done, the frame is loaded in the ring with the right colorspace, next step is temporal filtering
     }
     template <InputMemType T>
     double run(const uint8_t *dstp, int64_t dststride, const uint8_t* srcp1[3], const uint8_t* srcp2[3], int64_t stride, int64_t stride2){
         loadImageToRing<T>(srcp1, srcp2, stride, stride2);
-        return CVVDPprocess(dstp, dststride, temporalRing1, temporalRing2, model, maxshared, stream);
+        return CVVDPprocess(dstp, dststride, temporalRing1, temporalRing2, model, maxshared, stream1, stream2, event);
     }
     //empties the history.
     void flushTemporalRing(){
